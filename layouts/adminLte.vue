cb8cfa99d6a1c177740a7978540e3311fbc7ef0d
Befkadu Belete <befkadu@etmsoftwareplc.com>
2022-12-18 16:02:44 +0300

    first commit

---
+++ layouts/adminLte.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <NavBar />
+    <SideBar />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/pages/NavBar";
+import SideBar from "../components/pages/SideBar";
+import Footer from "../components/pages/Footer";
+export default {
+  components: {
+    NavBar,
+    SideBar,
+    Footer,
+  },
+};
+</script>
